abdf99d17865003737fa04f953a3ce171a2ec771
mattholamieux <matthew.nelson@Matthews-MacBook-Pro.local>
2023-03-04 13:25:35 -0800

    many changes on exp branch

--- main.js
+++ main.js
@@ -1,21 +1,70 @@
 let scroll;
+let bgSize = 90;
+let lastScroll = 0;
+let scrollDir = 1;
+let viewportHeight = window.innerHeight;
 
-window.onscroll = function(e) {
-    scroll = window.scrollY // Get scroll position
-    console.log(scroll);
-}
 
-// $(document).ready(function() {
 
-// $('.filters-list a').on('click', function(){
+$(document).ready(function() {
 
-//   var filter = $(this).attr('data-filter');
-//   $('.photo').hide();
-//   $(filter).show();
-//   $('.filters-list a').removeClass('selected');
-//   $(this).addClass('selected');
+    const body = document.getElementById('body');
+    body.style.backgroundSize = `${bgSize}%`;
 
-//   return false;
-// })
+    const firstAudio = document.getElementById('first-audio');
+    let firstAudioOffset = firstAudio.getBoundingClientRect();
+    let firstAudioTop = firstAudioOffset.top;
 
-// });
+    const lastAudio = document.getElementById('last-audio');
+    let lastAudioOffset = lastAudio.getBoundingClientRect();
+    let lastAudioTop = lastAudioOffset.top;
+
+    const firstWeb = document.getElementById('first-web');
+    let firstWebOffset = firstWeb.getBoundingClientRect();
+    let firstWebTop = firstWebOffset.top;
+
+    const lastWeb = document.getElementById('last-web');
+    let lastWebOffset = lastWeb.getBoundingClientRect();
+    let lastWebTop = lastWebOffset.top;
+
+    window.onscroll = function(e) {
+        scroll = window.scrollY // Get scroll position
+        body.style.backgroundSize = `${bgSize}%`;
+
+        if (scroll > 0 && lastAudioTop > 0) {
+            body.style.backgroundImage = "url(assets/audio.svg)"
+            if (scrollDir === 1 && bgSize > 50) {
+                bgSize--;
+            } else if (scrollDir === -1 && bgSize < 90 && firstAudioTop > viewportHeight) {
+                bgSize++;
+            }
+        } else if (lastAudioTop < 0 && lastWebTop > 0) {
+            body.style.backgroundImage = "url(assets/web.svg)"
+        } else if (lastWebTop < 0) {
+            body.style.backgroundImage = "url(assets/teaching.svg)"
+        }
+
+        firstAudioOffset = firstAudio.getBoundingClientRect();
+        firstAudioTop = firstAudioOffset.top;
+
+        lastAudioOffset = lastAudio.getBoundingClientRect();
+        lastAudioTop = lastAudioOffset.top;
+
+        firstWebOffset = firstWeb.getBoundingClientRect();
+        firstWebTop = firstWebOffset.top;
+
+        lastWebOffset = lastWeb.getBoundingClientRect();
+        lastWebTop = lastWebOffset.top;
+
+        console.log(firstWebTop);
+
+        if (scrollY > lastScroll) {
+            scrollDir = 1;
+            // lastScroll = scrollY;
+        } else {
+            scrollDir = -1;
+        }
+        lastScroll = scrollY;
+    }
+
+});
