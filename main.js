b9ef6a4001b235333e404a615fbd29ae7b299efd
mattholamieux <matthew.nelson@Matthews-MacBook-Pro.local>
2023-03-03 19:33:02 -0800

    changing thinz up

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
-$(document).ready(function() {
+let scroll;
 
-$('.filters-list a').on('click', function(){
+window.onscroll = function(e) {
+    scroll = window.scrollY // Get scroll position
+    console.log(scroll);
+}
 
-  var filter = $(this).attr('data-filter');
-  $('.photo').hide();
-  $(filter).show();
-  $('.filters-list a').removeClass('selected');
-  $(this).addClass('selected');
+// $(document).ready(function() {
 
-  return false;
-})
+// $('.filters-list a').on('click', function(){
 
-});
+//   var filter = $(this).attr('data-filter');
+//   $('.photo').hide();
+//   $(filter).show();
+//   $('.filters-list a').removeClass('selected');
+//   $(this).addClass('selected');
+
+//   return false;
+// })
+
+// });
